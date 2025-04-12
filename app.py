81a26f3e063af3d0993423d40a751b70ad59ad48
Shimal Akmal D <shimalakmal12@gmail.com>
2025-04-12 16:03:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -369,4 +369,5 @@ def payment_callback():
 
 # 🔥 Run Flask App
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
